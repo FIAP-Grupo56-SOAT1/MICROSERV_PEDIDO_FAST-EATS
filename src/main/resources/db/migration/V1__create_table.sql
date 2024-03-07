361ed232c360657b5e6fc4859b48d29c9e8cbf30
wilian.penaforte@hotmail.com <wilian.penaforte@hotmail.com>
2024-03-07 10:25:04 -0300

    alteração da tabela de cliente e das classes de cliente no repositorio e no core.

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS `categorias`
     `descricao` varchar(400) DEFAULT NULL,
     `ativo`     tinyint(1) NOT NULL,
     PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `produtos`;
 CREATE TABLE IF NOT EXISTS `produtos`
@@ -24,9 +24,9 @@ CREATE TABLE IF NOT EXISTS `produtos`
     PRIMARY KEY (`id`),
     KEY `tipoProdutoId_fk` (`categoriaId_fk`),
     CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`categoriaId_fk`) REFERENCES `categorias` (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `clientes`;
 CREATE TABLE IF NOT EXISTS `clientes`
@@ -38,9 +38,9 @@ CREATE TABLE IF NOT EXISTS `clientes`
     `email`        varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
     `ativo`        tinyint(1)                                                    DEFAULT NULL,
     PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `statuspedidos`;
 CREATE TABLE IF NOT EXISTS `statuspedidos`
@@ -49,15 +49,15 @@ CREATE TABLE IF NOT EXISTS `statuspedidos`
     `nome`  varchar(200) DEFAULT NULL,
     `ativo` tinyint(1) NOT NULL,
     PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `pedidos`;
 CREATE TABLE IF NOT EXISTS `pedidos`
 (
     `id`                  bigint NOT NULL AUTO_INCREMENT,
-    `clienteId_fk`        varchar(11) DEFAULT NULL,
+    `clienteId_fk`        bigint      DEFAULT NULL,
     `ativo`               tinyint(1)  DEFAULT NULL,
     `statusPedidoId_fk`   bigint      DEFAULT NULL,
     `dataHoraCriado`      datetime    DEFAULT NULL,
@@ -67,11 +67,11 @@ CREATE TABLE IF NOT EXISTS `pedidos`
     PRIMARY KEY (`id`),
     KEY `clienteId_fk` (`clienteId_fk`),
     KEY `statusPedidoId_fk` (`statusPedidoId_fk`),
-    CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`clienteId_fk`) REFERENCES `clientes` (`cpf`),
+    CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`clienteId_fk`) REFERENCES `clientes` (`id`),
     CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`statusPedidoId_fk`) REFERENCES `statuspedidos` (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `produtosdeumpedido`;
 CREATE TABLE IF NOT EXISTS `produtosdeumpedido`
@@ -85,6 +85,6 @@ CREATE TABLE IF NOT EXISTS `produtosdeumpedido`
     KEY `produtoId_fk` (`produtoId_fk`),
     CONSTRAINT `produtosdeumpedido_ibfk_1` FOREIGN KEY (`pedidoId_fk`) REFERENCES `pedidos` (`id`),
     CONSTRAINT `produtosdeumpedido_ibfk_2` FOREIGN KEY (`produtoId_fk`) REFERENCES `produtos` (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
