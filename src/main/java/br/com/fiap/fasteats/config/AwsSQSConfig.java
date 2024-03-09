6c0f1c0ea1ed9d18eb1c7dfdb8a3a23e0a7db6be
Jairo <hardtelles@gmail.com>
2024-03-09 18:07:32 -0300

    properties

--- src/main/java/br/com/fiap/fasteats/config/AwsSQSConfig.java
+++ src/main/java/br/com/fiap/fasteats/config/AwsSQSConfig.java
@@ -1,10 +1,13 @@
 package br.com.fiap.fasteats.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -15,26 +18,35 @@ import java.net.URI;
 @Configuration
 public class AwsSQSConfig {
 
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
     @Value("${spring.cloud.aws.sqs.endpoint}")
     private String endpoint;
 
+    //@Value("${spring.cloud.aws.credentials.session-token}")
+    //private String sessionToken;
+
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
-
-        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+        AWSCredentials credentials = new DefaultAWSCredentialsProviderChain().getCredentials();
+        String accessKey = credentials.getAWSAccessKeyId();
         String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
-        String sessionToken = System.getenv("AWS_SESSION_TOKEN");
+        //String sessionToken = System.getenv("AWS_SESSION_TOKEN");
         String region = System.getenv("AWS_REGION");
         System.out.println("region: " + region);
         System.out.println("accessKey: " + accessKey);
         System.out.println("secretKey: " + secretKey);
-        System.out.println("sessionToken: " + sessionToken);
+        //System.out.println("sessionToken: " + sessionToken);
         return SqsAsyncClient
                 .builder()
                 .endpointOverride(URI.create(endpoint))
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider
-                        .create(AwsSessionCredentials.create(accessKey, secretKey, sessionToken)))
+                        .create(AwsBasicCredentials.create(this.accessKey, this.secretKey)))
                 .build();
     }
 
