ef3670d676b0acaff0388070668c5c7679c389fb
Jairo <hardtelles@gmail.com>
2024-03-10 11:20:56 -0300

    session token

--- src/main/java/br/com/fiap/fasteats/config/AwsSQSConfig.java
+++ src/main/java/br/com/fiap/fasteats/config/AwsSQSConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -18,6 +19,8 @@ public class AwsSQSConfig {
     private String accessKey;
     @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
+    @Value("${spring.cloud.aws.credentials.session-token}")
+    private String sessionToken;
     @Value("${spring.cloud.aws.region.static}")
     private String region;
     @Value("${spring.cloud.aws.sqs.endpoint}")
@@ -31,7 +34,7 @@ public class AwsSQSConfig {
                 .endpointOverride(URI.create(endpoint))
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider
-                        .create(AwsBasicCredentials.create(accessKey, secretKey)))
+                        .create(AwsSessionCredentials.create(accessKey, secretKey,sessionToken)))
                 .build();
     }
 
